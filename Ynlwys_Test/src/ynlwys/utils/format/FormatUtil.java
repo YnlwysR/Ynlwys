bad0db751698b3dba8de1fcf4e617f32589f0138
YnlwysR <1149258184@qq.com>
2015-09-11 15:34:46 +0800

    修改,添加所有工具方法的注释.

--- Ynlwys_Test/src/ynlwys/utils/format/FormatUtil.java
+++ Ynlwys_Test/src/ynlwys/utils/format/FormatUtil.java
@@ -7,91 +7,81 @@ package ynlwys.utils.format;
  * 
  * 介绍:
  * 		0,该工具类中包含了常规的和非常规的格式化方法.
- * 
+ * 		1,当中包含的全部都是静态方法,直接通过类名调用即可.
  * 
  * @author LiYinghao
  *
  */
+
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
-/**
- * 格式化工具类 - 当中可以对多种数据进行格式化.
- * 
- * 时间:2015年6月2日09:42:26.
- * 	
- * 		更新:
- * 			0,判断传递的日期是不是上一个月.
- * 
- * @author LiYinghao
- *
- */
+
 public class FormatUtil {
+	
+	/**
+	 * 将一个Long类型的时间值变成 'yyyy-MM-dd' 格式类型的时间值 年月日格式化.
+	 * @param time Long类型(毫秒)的时间值.
+	 * @return String类型的 'yyyy-MM-dd' 格式的时间值.
+	 */
 	public static final String formatDate(Long time){
 		if(time == null)
 			return null;
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		return df.format(new Date(time));
 	}
+	
+	/**
+	 * 将一个Long类型的时间值变成 'HH:mm:ss' 格式类型的时间值 时分秒格式化.
+	 * @param time Long类型(毫秒)的时间值.
+	 * @return String类型的 'HH:mm:ss' 格式的时间值.
+	 */
 	public static final String formatTime(Long time){
 		if(time == null)
 			return null;
 		DateFormat df = new SimpleDateFormat("HH:mm:ss");
 		return df.format(new Date(time));
 	}
+	
+	/**
+	 * 将一个Long类型的时间值变成 'yyyy-MM-dd HH:mm:ss' 格式类型的时间值 年月日,时分秒格式化.
+	 * @param time Long类型(毫秒)的时间值.
+	 * @return String类型的 'yyyy-MM-dd HH:mm:ss' 格式的时间值.
+	 */
 	public static final String formatDateTime(Long time){
 		if(time == null)
 			return null;
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return df.format(new Date(time));
 	}
+	
+	/**
+	 * 将字符串的日期类型转换为毫秒值的时间值.
+	 * @param date String 类型的日期值 格式为'yyyy-MM-dd' 
+	 * @return Long 类型的日期值.
+	 * @throws ParseException
+	 */
+	public static long getLongDateByStringDate(String date) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateTemp = null;
+		try {
+			dateTemp = simpleDateFormat.parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}        
+		return dateTemp.getTime();
+	}
+	
+	/**
+	 * 金钱格式化,格式化Double类型的数据为保留小数后两位.
+	 * @param money 需要格式化的值.
+	 * @return String 类型的保留两位小数的值.
+	 */
 	public static final String formatMoney(Double money) {
 		DecimalFormat df = new DecimalFormat("#0.00");
 		return df.format(money);
-	}
-	/*
-	 * 将字符串日期转换成,毫秒值也就是long型数值.
-	 * **/
-	public static long getdaytime(String date) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateTemp = null;
-        try {
-        	dateTemp = simpleDateFormat.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }        
-        return dateTemp.getTime();
-    }
-	
-	/**
-	 * 判断请假日期是不是上一个月 - 如果系统时间得到的是0.
-	 */
-	public static Boolean isAlike(String leaveDate) {
-		Calendar calendar = Calendar.getInstance();
-		
-		String month = leaveDate.split("-")[1];
-		String year = leaveDate.split("-")[0];
-		String monthTemp = calendar.get(Calendar.MONTH) < 10 ? "0"+(calendar.get(Calendar.MONTH)) : (calendar.get(Calendar.MONTH)) + "";
-		String yearTemp = calendar.get(Calendar.YEAR) + "";
-		
-		/**#如果取出来的是00那么就认为是12 - 1#*/
-		if(monthTemp.equals("00")) {
-			monthTemp = "12";
-		}
-		
-		if(month.equals(monthTemp) && year.equals(yearTemp)) {
-			return true;
-		}else {
-			return false;
-		}
-			
-	}
-	public static String formatDay(double parseDouble) {
-		DecimalFormat df = new DecimalFormat("#0.000");
-		return df.format(parseDouble);
 	}
 }
